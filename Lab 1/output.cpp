69b1ec89feb51a1e1f675038189d72e79a3f9923
Ruchi-23 <ruchira2306@gmail.com>
2022-02-04 12:20:08 +0530

    first commit

---
+++ Lab 1/output.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    void test1();
+    void test2();
+    test1();
+    return 0;
+}
+void test1(){
+    test2();
+    cout<<"\nOne";
+}
+void test2(){
+    cout<<"\nTwo";
+}
